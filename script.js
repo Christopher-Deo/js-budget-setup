b1535f50e8606829e8a89d13e463f4ee63535467
Christopher Deo <110053896+Christopher-Deo@users.noreply.github.com>
2022-10-13 22:24:42 -0500

    completed JS file

--- script.js
+++ script.js
@@ -130,6 +130,45 @@ if(this.itemList > 0){
     return total;
 }
 
+//method to edit a listed expense
+editExpense(element){
+  //getting the id of the element to be edited
+  let id = parseInt(element.dataset.id)//getting the dataset-id from the html file
+  //traversing the DOM tp get the correct parent element.
+  let parent = element.parentElement.parentElement.parentElement;
+  //removing it from the dom
+  this.expenseList.removeChild(parent);
+  // filtering the expense item to be edited from the list (array)
+  let expense = this.itemList.filter(function(item){
+    return item.id === id;
+  })
+
+  //show value
+  this.expenseInput.value = expense[0].title;
+  this.amountInput.value = expense[0].amount;
+
+  // remove the item from the expense list
+  let tempList = this.itemList.filter(function(item){
+    return item.id !== id;
+  })
+  this.itemList = tempList;
+  this.showBalance();
+}
+
+//method to delete an expense from the list
+deleteExpense(element){
+  let id = parseInt(element.dataset.id);
+  let parent = element.parentElement.parentElement.parentElement;
+  //remove from DOM
+  this.expenseList.removeChild(parent);
+  //remove from list
+  let tempList = tjos.itemList.filter(function(item){
+    return item.id !== id;
+  });
+  this.itemList = tempList;
+  this.showBalance();
+
+}
 
 
 }  //end of UI class
@@ -155,9 +194,13 @@ expenseForm.addEventListener('submit', function(event){
 })
 
 //expense click
-expenseList.addEventListener('click', function(){
-
-})
+expenseList.addEventListener('click', function(event){
+  if(event.target.parentElement.classList.contains('edit-icon')){
+    ui.editExpense(event.target.parentElement)
+  }else if (event.target.parentElement.classList.contains('delete-icon')){
+    ui.deleteExpense(event.target.parentElement)
+  }
+});
 
 }
 
